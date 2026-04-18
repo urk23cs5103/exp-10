6cd18e55975b619b03834dcdc1081f74449b16a7
urk23cs5103 <156046125+urk23cs5103@users.noreply.github.com>
2026-04-18 09:46:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+name to clipboard
++3
+Lines changed: 3 additions & 0 deletions
+Original file line number	Diff line number	Diff line change
+@@ -0,0 +1,3 @@
+function showMsg() {
+    alert("AI Generated Website!");
+}
